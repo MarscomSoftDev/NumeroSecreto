c0767cc2835ac90a41b59c3fbd63b5ab80ee717b
marscom <marscompereira@gmail.com>
2025-01-26 23:37:29 -0500

    contador

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-//variables;
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 
 while (numeroUsuario != numeroSecreto) {
   numeroUsuario = parseInt(prompt("Indica un numero por favor entre 1 y 10"));
+  console.log(numeroUsuario);
+
   if (numeroUsuario == numeroSecreto) {
-    console.log(`acertaste, el numero es : ${numeroSecreto}`);
+    console.log(`acertaste, el numero es : ${numeroUsuario}`);
   } else {
     if (numeroUsuario > numeroSecreto) {
       console.log("el numero secreto es menor");
